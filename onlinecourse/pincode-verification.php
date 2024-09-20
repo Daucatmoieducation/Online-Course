5d005ac6735837636cba23aaec3da90c3c648a8a
TuanNguyen <daucatmoieducation@gmail.com>
2024-09-20 09:05:46 +0700

    sua xong file pincode-verification.php

--- onlinecourse/pincode-verification.php
+++ onlinecourse/pincode-verification.php
@@ -19,7 +19,7 @@ if($num>0)
 $_SESSION['pcode']=$_POST['pincode'];
 header("location:enroll.php");
 }else{
-echo '<script>alert("Error :Wrong Pincode. Please Enter a Valid Pincode !!")</script>';
+echo '<script>alert("Error: Sai Pincode. Vui lòng nhập lại!!")</script>';
 echo '<script>window.location.href=my-pincode-verification.php</script>';    
 }
 }
@@ -32,7 +32,7 @@ echo '<script>window.location.href=my-pincode-verification.php</script>';
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
     <meta name="description" content="" />
     <meta name="author" content="" />
-    <title>Pincode Verification</title>
+    <title>Xác minh Pincode</title>
     <link href="assets/css/bootstrap.css" rel="stylesheet" />
     <link href="assets/css/font-awesome.css" rel="stylesheet" />
     <link href="assets/css/style.css" rel="stylesheet" />
@@ -46,45 +46,39 @@ echo '<script>window.location.href=my-pincode-verification.php</script>';
  include('includes/menubar.php');
 }
  ?>
-    <!-- MENU SECTION END-->
-    <div class="content-wrapper">
-        <div class="container">
-              <div class="row">
-                    <div class="col-md-12">
-                        <h1 class="page-head-line">Student Pincode Verification</h1>
+   <!-- MENU SECTION END -->
+<div class="content-wrapper">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h1 class="page-head-line">XÁC MINH MÃ SINH VIÊN</h1>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-3"></div>
+            <div class="col-md-6">
+                <div class="panel panel-default">
+                    <font color="red" align="center">
+                        <?php echo htmlentities($_SESSION['msg']);?>
+                        <?php echo htmlentities($_SESSION['msg']="");?>
+                    </font>
+
+                    <div class="panel-body">
+                        <form name="pincodeverify" method="post">
+                            <div class="form-group">
+                                <label for="pincode">Nhập Pincode</label>
+                                <input type="password" class="form-control" id="pincode" name="pincode" placeholder="Pincode" required />
+                            </div>
+
+                            <button type="submit" name="submit" class="btn btn-default">Confirm</button>
+                            <hr />
+                        </form>
                     </div>
                 </div>
-                <div class="row" >
-                  <div class="col-md-3"></div>
-                    <div class="col-md-6">
-                        <div class="panel panel-default">
-                        <div class="panel-heading">
-                          Pincode Verification
-                        </div>
-<font color="red" align="center"><?php echo htmlentities($_SESSION['msg']);?><?php echo htmlentities($_SESSION['msg']="");?></font>
-
-
-                        <div class="panel-body">
-                       <form name="pincodeverify" method="post">
-   <div class="form-group">
-    <label for="pincode">Enter Pincode</label>
-    <input type="password" class="form-control" id="pincode" name="pincode" placeholder="Pincode" required />
-  </div>
- 
-  <button type="submit" name="submit" class="btn btn-default">Verify</button>
-                           <hr />
-   
-
-
-
-</form>
-                            </div>
-                            </div>
-                    </div>
-                  
-                </div>
+            </div>
         </div>
     </div>
+</div>
     <!-- CONTENT-WRAPPER SECTION END-->
   <?php include('includes/footer.php');?>
     <!-- FOOTER SECTION END-->
